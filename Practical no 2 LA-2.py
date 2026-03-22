e3e4c8f0d378b8cea5dafd8909472330a0ac1511
vp7197763-hash <vp7197763@gmail.com>
2026-03-22 23:57:34 +0530

    Add files via upload

---
+++ Practical no 2 LA-2.py
@@ -0,0 +1,23 @@
+hardness = float(input("Enter hardness of steel: "))
+carbon = float(input("Enter carbon content: "))
+tensile = float(input("Enter tensile strength: "))
+
+# Checking conditions
+c1 = hardness > 50
+c2 = carbon < 0.7
+c3 = tensile > 5600
+
+if c1 and c2 and c3:
+    grade = 10
+elif c1 and c2:
+    grade = 9
+elif c2 and c3:
+    grade = 8
+elif c1 and c3:
+    grade = 7
+elif c1 or c2 or c3:
+    grade = 6
+else:
+    grade = 5
+
+print("Grade of the steel is:", grade)
